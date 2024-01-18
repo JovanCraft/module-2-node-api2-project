7d59dcc545fbb4285ab31fd831e65eceb15b2f5a
jovan <jovancraft@gmail.com>
2024-01-18 03:05:20 -0600

    created the server and required it into indes.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,8 @@
 // implement your posts router here
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/api/posts', (req, res) => {
+    res.status(200).json()
+})
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+
+const server = require('./api/server')
+
+const port = 8000
+
+server.listen(port, () => console.log(`All the right stuff is happening over on port ${port}!!`))
